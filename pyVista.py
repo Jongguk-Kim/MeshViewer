c2985fa0dcf2ab0676fab9c27a3fcd6ac5bc276c
Jongguk-Kim <whdrnr25@gmail.com>
2022-02-26 15:24:35 +0900

    view option

--- pyVista.py
+++ pyVista.py
@@ -1,6 +1,20 @@
-import pyvista as pv
+try: 
+    import pyvista as pv
+except: 
+    os.system("pip install pyvista")
+    
 import vtk 
 import numpy as np 
+
+class MESH(): 
+    def __init__(self, meshfile, centering=False) : 
+        self.meshfile = meshfile 
+        
+        self.grid, self.edges, self.pt_cloud, self.surfaces, self.nodes, self.idx_element, self.cells  = \
+            load_pyVista_mesh(meshfile, centering=centering)
+
+        
+
 
 def readInp(fname): 
 
@@ -10,8 +24,14 @@ def readInp(fname):
     s8=[]
     cmd = False 
     offset = 0 
+    index_elements=[]
+    no_changeXZ = True 
+    mtype = 9
     for line in lines: 
-        if "**" in line: continue 
+        if "**" in line: 
+            if "END OF MATERIAL INFO" in line: 
+                no_changeXZ = False 
+            continue 
         # if "TREADPTN_NIDSTART_NIDOFFSET_EIDSTART_EIDOFFSET" in line.upper(): 
         #     wds = line.split(",")
         #     offset = int(wds[-1].strip())
@@ -25,33 +45,63 @@ def readInp(fname):
                 cmd = 'n8'
             elif "*ELEMENT" in line.upper() and "C3D6" in line.upper(): 
                 cmd = 'n6'
+            elif "*ELEMENT" in line.upper() and "MGAX1" in line.upper(): 
+                cmd = 'n2'
+            elif "*ELEMENT" in line.upper() and "CGAX3" in line.upper(): 
+                cmd = 'n3'
+            elif "*ELEMENT" in line.upper() and "CGAX4" in line.upper():
+                mtype =  5 
+                cmd = 'n4'
             else: 
                 cmd = False 
         else: 
             wds = line.split(",")
             if cmd == 'n': 
-                nodes.append([ float(wds[0].strip()), float(wds[3].strip()), float(wds[2].strip()), float(wds[1].strip())  ])
+                if no_changeXZ: 
+                    nodes.append([ float(wds[0].strip()), float(wds[1].strip()), float(wds[2].strip()), float(wds[3].strip())  ])
+                    
+                else: 
+                    nodes.append([ float(wds[0].strip()), float(wds[3].strip()), float(wds[2].strip()), float(wds[1].strip())  ])
             if cmd == 'n8':
                 s8.append([8, 
                             int(wds[1].strip()), int(wds[2].strip()), int(wds[3].strip()), int(wds[4].strip()), 
                             int(wds[5].strip()), int(wds[6].strip()), int(wds[7].strip()), int(wds[8].strip())
-                            ]) 
+                            ])
+                index_elements.append([int(wds[0].strip()), len(s8)]) 
             if cmd == 'n6':
                 s8.append([8, 
                             int(wds[1].strip()), int(wds[2].strip()), int(wds[3].strip()), int(wds[3].strip()), 
                             int(wds[4].strip()), int(wds[5].strip()), int(wds[6].strip()), int(wds[6].strip())
                             ]) 
-    return nodes, s8
+                index_elements.append([int(wds[0].strip()), len(s8)]) 
+            
+            # if cmd == 'n2':
+            #     s8.append([4, 
+            #                 int(wds[1].strip()), int(wds[1].strip()), int(wds[2].strip()), int(wds[2].strip())
+            #               ]) 
+            #     index_elements.append([int(wds[0].strip()), len(s8)]) 
+            # if cmd == 'n3':
+            #     s8.append([4, 
+            #                 int(wds[1].strip()), int(wds[2].strip()), int(wds[3].strip()), int(wds[3].strip())
+            #               ]) 
+            #     index_elements.append([int(wds[0].strip()), len(s8)]) 
+            if cmd == 'n4':
+                s8.append([4, 
+                            int(wds[1].strip()), int(wds[2].strip()), int(wds[3].strip()), int(wds[4].strip())
+                          ]) 
+                index_elements.append([int(wds[0].strip()), len(s8)]) 
+
+    return nodes, s8, index_elements, mtype
 
 def readMesh_pyVista(fname,  files=None, centering=False): 
     
-    nodes, s8 = readInp(fname)
+    nodes, s8, idx_element, meshtype = readInp(fname)
     nodes = np.array(nodes)
     s8 = np.array(s8)
 
     if not isinstance(files, type(None)): 
         for file in files: 
-            nds, sd = readInp(file) 
+            nds, sd, idx_element, meshtype = readInp(file) 
             nodes = np.concatenate((nodes, np.array(nds)), axis=0)
             s8 = np.concatenate((s8, np.array(sd)), axis=0)
 
@@ -72,33 +122,31 @@ def readMesh_pyVista(fname,  files=None, centering=False):
         npn[int(n[0])][1]=n[1]
         npn[int(n[0])][2]=n[2]
         npn[int(n[0])][3]=n[3]
-    return npn, np.array(s8).ravel()
+    return npn, np.array(s8).ravel(), idx_element, s8, meshtype
 
 
-def readSmallMesh_pyVista(fname): 
-    nodes, s8 = readInp(fname)
-    nodes = np.array(nodes)
-    s8 = np.array(s8)
+# def readSmallMesh_pyVista(fname): 
+#     nodes, s8, idx_element, meshtype = readInp(fname)
+#     nodes = np.array(nodes)
+#     s8 = np.array(s8)
 
-    solids=[]
-    for s in s8: 
-        ix1=np.where(nodes[:,0] == s[1])[0][0]
-        ix2=np.where(nodes[:,0] == s[2])[0][0]
-        ix3=np.where(nodes[:,0] == s[3])[0][0]
-        ix4=np.where(nodes[:,0] == s[4])[0][0]
-        ix5=np.where(nodes[:,0] == s[5])[0][0]
-        ix6=np.where(nodes[:,0] == s[6])[0][0]
-        ix7=np.where(nodes[:,0] == s[7])[0][0]
-        ix8=np.where(nodes[:,0] == s[8])[0][0]
-        solids.append([8, ix1, ix2, ix3, ix4, ix5, ix6, ix7, ix8]) 
+#     solids=[]
+#     for s in s8: 
+#         ix1=np.where(nodes[:,0] == s[1])[0][0]
+#         ix2=np.where(nodes[:,0] == s[2])[0][0]
+#         ix3=np.where(nodes[:,0] == s[3])[0][0]
+#         ix4=np.where(nodes[:,0] == s[4])[0][0]
+#         ix5=np.where(nodes[:,0] == s[5])[0][0]
+#         ix6=np.where(nodes[:,0] == s[6])[0][0]
+#         ix7=np.where(nodes[:,0] == s[7])[0][0]
+#         ix8=np.where(nodes[:,0] == s[8])[0][0]
+#         solids.append([8, ix1, ix2, ix3, ix4, ix5, ix6, ix7, ix8]) 
 
-    return nodes, np.array(solids).ravel()
+#     return nodes, np.array(solids).ravel(), meshtype
     
 
-def load_pyVista_mesh(file_name, centering=False): 
-
-    nodes, cells = readMesh_pyVista(file_name, centering=centering)
-    nCell = int(len(cells)/9)
+def makePyvisterCells(cells, nodes, meshtype): 
+    nCell = int(len(cells)/meshtype)
 
     xyz = nodes[:,1:4]
 
@@ -110,25 +158,38 @@ def load_pyVista_mesh(file_name, centering=False):
     # grid = pv.UnstructuredGrid(offset8, cells, celltypes, xyz) 
 
     #  if you are using VTK 9.0 or newer, you do not need to input the offset array:
-    grid = pv.UnstructuredGrid(cells, celltypes, xyz)
+    # grid = pv.UnstructuredGrid(cells, celltypes, xyz)
 
-    # Alternate versions:
-    grid = pv.UnstructuredGrid({vtk.VTK_HEXAHEDRON: cells.reshape([-1, 9])[:, 1:]}, xyz)
-    grid = pv.UnstructuredGrid({vtk.VTK_HEXAHEDRON: np.delete(cells, np.arange(0, cells.size, 9))}, xyz)
+    # # Alternate versions:
+    # grid = pv.UnstructuredGrid({vtk.VTK_HEXAHEDRON: cells.reshape([-1, meshtype])[:, 1:]}, xyz)
+    grid = pv.UnstructuredGrid({vtk.VTK_HEXAHEDRON: np.delete(cells, np.arange(0, cells.size, meshtype))}, xyz)
+
+    return grid, xyz  
+
+def load_pyVista_mesh(file_name, centering=False): 
+
+    nodes, cells, idx_element, elements, meshtype = readMesh_pyVista(file_name, centering=centering)
+    grid, xyz = makePyvisterCells(cells, nodes, meshtype)
 
     pt_cloud = pv.PolyData(xyz)
-    # edges = grid.extract_all_edges()
-    edges = grid.extract_feature_edges(feature_angle=45, boundary_edges=False)
-    surfaces = grid.extract_surface()
-    print(" Mesh Volume", grid.volume)
-    return grid, edges, pt_cloud, surfaces, nodes
+    if meshtype ==9: 
+        edges = grid.extract_feature_edges(feature_angle=45, boundary_edges=False)
+        surfaces = grid.extract_surface()
+        print(" Mesh Volume", grid.volume)
+    else: 
+        edges = grid.extract_all_edges()
+        surfaces = None 
+    
+    return grid, edges, pt_cloud, surfaces, nodes, idx_element, elements
 
 def lighting(): 
     # setup lighting
     lights=[]
     # light = pv.Light((-2, 2, 0), (0, 0, 0), 'white'); lights.append(light)
-    # light = pv.Light((1, 1, 1), (0, 0, 0), 'white'); lights.append(light)
+    light = pv.Light((1, 1, 1), (0, 0, 0), 'white'); lights.append(light)
+    light = pv.Light((-1, -1, -1), (0, 0, 0), 'white'); lights.append(light)
     light = pv.Light((1, 0, 0), (0, 0, 0), 'white'); lights.append(light)
+    light = pv.Light((-1, 0, 0), (0, 0, 0), 'gray'); lights.append(light)
     # light2 = pv.Light((2, 0, 0), (0, 0, 0), (0.7, 0.0862, 0.0549))
     # light3 = pv.Light((0, 0, 10), (0, 0, 0), 'white'); lights.append(light)
 
@@ -137,6 +198,35 @@ def lighting():
 def createCamera(): 
     return pv.Camera()
 
+def generatePointCloud(ids, npns): 
+    points =[]; tlabels=[]
+    for npn in npns: 
+        for id in ids: 
+            if npn[id][0] != 0: 
+                points.append(npn[id][1:4])
+                tlabels.append(str(int(npn[id][0])))
+    poly_points=pv.PolyData(np.array(points))
+    poly_points["label"] = [f'{tlabels[i]}' for i in range(poly_points.n_points)]
+    
+    return  poly_points
+
+def generateMesh_searched(ids, indexes, elements, nodes): 
+    
+    cells =[]
+    indexes = np.array(indexes)
+    elements = np.array(elements) 
+    cnt = 0 
+    for id in ids: 
+        ix = np.where(indexes[:,0] == id)[0]
+        if len(ix) > 0: 
+            for x in ix: 
+                cells.append(elements[indexes[x][1]])
+                cnt += 1 
+    if cnt > 0: 
+        grid, _ = makePyvisterCells(np.array(cells).ravel(), nodes, 9)
+        return  grid 
+    else: 
+        return None 
 # def getCameraAngle(camera): 
 
 
